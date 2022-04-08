b485d74b29135fb1f0ac6196b7e65ad81c8119bd
Corey <corey.hampton@student.awesomeinc.org>
2022-04-08 12:45:05 +0000

    Add CRUD

--- demo.py
+++ demo.py
@@ -49,15 +49,34 @@ def create_hero():
     """.format(name, about, bio)
 )
 
-def Update_hero():
+def update_hero():
     name = input("Enter a Name\n> ")
-    selection = ("What would you like to change?\n\n1. Name\n2. About Me\n3. Bio\n4. Abilities\n5. Relationships\n> ")
-    execute_query( """
-    insert into heroes(name, about_me, biography) (
-        values('{}','{}','{}')
-    )
-    """.format(name, about, bio)
-    )
+    selection = input("What would you like to change?\n\n1. Name\n2. About Me\n3. Bio\n4. Abilities\n5. Relationships\n> ")
+    if selection == '1':
+        oldname = name
+        name = input("Enter a New Name\n> ")
+        execute_query( """
+        UPDATE heroes
+        SET name = '{}'
+        WHERE name = '{}';
+        """.format(name, oldname)
+        )
+    if selection == '2':
+        about = input("Enter a New About Me\n> ")
+        execute_query( """
+        UPDATE heroes
+        SET about_me = '{}'
+        WHERE name = '{}';
+        """.format(about, name)
+        )
+    if selection == '3':
+        bio = input("Enter a New Bio\n> ")
+        execute_query( """
+        UPDATE heroes
+        SET biography = '{}'
+        WHERE name = '{}';
+        """.format(bio, name)
+        )
 
 def delete_hero():
     name = input("Enter a Name: ")
@@ -70,53 +89,29 @@ def delete_hero():
 
 while True:
     print("\nWelcome to the Hero Database, what would you like to do?\n")
-    selection = input("Would you like to view Heroes or Abilities?\n> ")
-    if selection.lower() == 'heroes':
-        selection = input("1. View all Current Heroes\n2. Create a New Hero\n3. Update a Hero\n4. Remove a Hero\n5. Back to Main Menu\n> ")
-        while selection:
-            if selection == '1':
-                # print("You selected View all Current Heroes")
-                view_heroes()    
-                break
-            elif selection == '2':
-                # print("You selected Create a New Hero")
-                create_hero()
-                break
-            elif selection == '3':
-                # print("You selected Update a Hero")
-                break
-            elif selection == '4':
-                # print("You selected Remove a Hero")
-                delete_hero()
-                break
-            elif selection == '5':
-                break
-            else:
-                print("Something went wrong... Try again")
-                selection = input("Enter a number: ")
-    if selection.lower() == 'abilities':
-        selection = input("1. View all Current Abilities\n2. Create a New Ability\n3. Update an Ability\n4. Remove an Ability\n5. Back to Main Menu\n> ")
-        while selection:
-            if selection == '1':
-                # print("You selected View all Current Heroes")
-                view_abilities()    
-                break
-            elif selection == '2':
-                # print("You selected Create a New Hero")
-                create_ability()
-                break
-            elif selection == '3':
-                # print("You selected Update a Hero")
-                break
-            elif selection == '4':
-                # print("You selected Remove a Hero")
-                delete_ability()
-                break
-            elif selection == '5':
-                break
-            else:
-                print("Something went wrong... Try again")
-                selection = input("Enter a number: ")
+    selection = input("1. View all Current Heroes\n2. Create a New Hero\n3. Update a Hero\n4. Remove a Hero\n5. Back to Main Menu\n> ")
+    while selection:
+        if selection == '1':
+            # print("You selected View all Current Heroes")
+            view_heroes()    
+            break
+        elif selection == '2':
+            # print("You selected Create a New Hero")
+            create_hero()
+            break
+        elif selection == '3':
+            # print("You selected Update a Hero")
+            update_hero()
+            break
+        elif selection == '4':
+            # print("You selected Remove a Hero")
+            delete_hero()
+            break
+        elif selection == '5':
+            break
+        else:
+            print("Something went wrong... Try again")
+            selection = input("Enter a number: ")
         
     exit_check = input("\nExit? y/n\n> ")
     if exit_check.lower() == 'y':
